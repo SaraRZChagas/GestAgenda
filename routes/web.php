b1be9b4d56fa8d9cb2e17e4855224da87ac5a34d
Sara Chagas <sarareginaz.ch@gmail.com>
2025-07-22 09:28:06 +0100

    Criação de landpage welcome com funcionalidade de envio de email pela pagina de contactos de acordo com prototipo.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\ContactController;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -12,6 +13,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+
+Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
